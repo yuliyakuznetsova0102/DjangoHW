e00c144a18dc2e8ae148221d092ac5583c50de31
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-12-29 14:49:45 +0300

    /

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/src/home.html')
 
 
 def contacts_page(request):
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/src/contacts.html')
