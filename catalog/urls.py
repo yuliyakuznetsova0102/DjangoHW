b8be3b671188535885d10104a050ffcaa566499c
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2025-01-15 21:09:49 +0300

    fixing

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,9 @@ from catalog import views
 
 app_name = 'catalog'
 
+
 urlpatterns = [
-    path('home/', views.home_page, name='home'),
-    path('contacts/', views.contacts_page, name='contacts'),
-    path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
-    path('home/', views.product_list, name='product_list'),
+    path(' home/', views.home_page, name='home'),
+    path(' contacts/', views.contacts_page, name='contacts'),
+    path(' product_detail/<int:pk>/', views.product_detail, name='product_detail'),
 ]
